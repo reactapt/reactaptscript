2168aa1558f7c34007f5f6964ca6e783bba8c202
reactapt <devilshit667@gmail.com>
2024-05-15 00:04:09 +0300

    ETO KAIF

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -9,6 +9,7 @@ export const Main = observer(() => {
             <div>
                 <form onSubmit={formDataStore.handleSubmit}>
                     <input type="text" name="firstName" placeholder="Имя" value={formDataStore.formData.firstName} onChange={formDataStore.handleChange} required />
+                    <input type="text" name="secondName" placeholder="Фамилия" value={formDataStore.formData.secondName} onChange={formDataStore.handleChange} required />
                     <InputMask mask="+7 (999) 999-99-99" maskChar=" " type="text" name="phone" placeholder="Номер телефона" value={formDataStore.formData.phone} onChange={formDataStore.handleChange} required />
                     <input type="submit" value="Отправить" />
                 </form>
@@ -16,6 +17,7 @@ export const Main = observer(() => {
                 {formDataStore.formDataList.map((data, index) => (
                     
                         <div className="dan" key={index}>
+                            <p>Фамилия: {data.secondName}</p>
                             <p>Имя: {data.firstName}</p>
                             <p>Телефон: {data.phone}</p>
                             <button className="del" onClick={() => formDataStore.handleDelete(index)}>Удалить</button>
